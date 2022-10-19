7a74ad2f987ff9339eac7f36065e616f826385ad
ivan2798 <inge1998@hotmail.com>
2022-10-19 16:55:45 -0500

    Arrays IV

--- bootcamp_CSharp_lesson_8/Program.cs
+++ bootcamp_CSharp_lesson_8/Program.cs
@@ -17,18 +17,19 @@ namespace bootcamp_CSharp_lesson_8
 
             // var datos = new[] { "Bruno", "Díaz", 40 };
 
-            var valores = new[] { 15, 28, 35, 75.5, 50.0, 90}; //todo a double
+            var valores = new[] { 15, 28, 35, 75.5, 50.0, 30.30, 90}; //todo a double
 
             //array de objetos
 
             Empleados Ana = new Empleados("Ana", 400);
 
-            Empleados[] arrayEmpleados = new Empleados[2];
+            Empleados[] arrayEmpleados = new Empleados[3];
 
             arrayEmpleados[0] = new Empleados("Sara", 400);
 
             arrayEmpleados[1] = Ana;
 
+            arrayEmpleados[2] = new Empleados("Marco", 700);
             //array de tipos o clases anónimas
 
             var personas = new[]
@@ -40,9 +41,15 @@ namespace bootcamp_CSharp_lesson_8
                 new{Nombre="Jorge", Dinero=900}
             };
 
-            for (int i = 0; i <5; i++)
+            /*for (int i = 0; i <arrayEmpleados.Length; i++)
             {
-                Console.WriteLine(valores[i]);
+                Console.WriteLine(arrayEmpleados[i].getInfo());
+            }*/
+
+            foreach (var variable in personas)
+            {
+                Console.WriteLine(variable);
+
             }
 
         }
@@ -59,8 +66,13 @@ namespace bootcamp_CSharp_lesson_8
             this.dinero = dinero;   
         }
 
-        string nombre;
-        int dinero;
+        public string getInfo()
+        {
+            return "Nombre del empleado:" + nombre + "Salario:" + dinero;
+        }
+        private string nombre;
+
+        private int dinero;
     }
 
 }
