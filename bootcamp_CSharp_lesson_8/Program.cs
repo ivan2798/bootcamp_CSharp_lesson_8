fced7fd0456b073311349bf19d175973a83eaf9b
ivan2798 <inge1998@hotmail.com>
2022-10-19 12:36:41 -0500

    Arrays I

---
+++ bootcamp_CSharp_lesson_8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Security.Cryptography;
+
+
+
+// Hello World! program
+namespace bootcamp_CSharp_lesson_8
+{
+    class Program
+    {
+        // intellisense
+
+        static void Main(string[] args)
+        {
+            int[] edades;
+
+            edades = new int[4];
+
+            edades[0] = 15;
+
+            edades[1] = 27;
+
+            edades[2] = 19;
+
+            edades[3] = 80;
+
+            Console.WriteLine(edades[2]);
+            
+        }
+
+
+    }
+
+}
