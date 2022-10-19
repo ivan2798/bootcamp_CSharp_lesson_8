e9127ce141491a67ec1065ba89e6317cc0a24e4f
ivan2798 <inge1998@hotmail.com>
2022-10-19 17:13:54 -0500

    Arrays V

--- bootcamp_CSharp_lesson_8/Program.cs
+++ bootcamp_CSharp_lesson_8/Program.cs
@@ -13,46 +13,43 @@ namespace bootcamp_CSharp_lesson_8
 
         static void Main(string[] args)
         {
-            // array implícito
+            int[] arrayElementos = LeerDatos();
 
-            // var datos = new[] { "Bruno", "Díaz", 40 };
+            Console.WriteLine("Imprimiendo desde el main");
 
-            var valores = new[] { 15, 28, 35, 75.5, 50.0, 30.30, 90}; //todo a double
-
-            //array de objetos
-
-            Empleados Ana = new Empleados("Ana", 400);
-
-            Empleados[] arrayEmpleados = new Empleados[3];
-
-            arrayEmpleados[0] = new Empleados("Sara", 400);
-
-            arrayEmpleados[1] = Ana;
-
-            arrayEmpleados[2] = new Empleados("Marco", 700);
-            //array de tipos o clases anónimas
-
-            var personas = new[]
+            foreach (int i in arrayElementos)
             {
-                new{Nombre="Juan", Dinero=19},
-
-                new{Nombre="Marcos", Dinero=500},
-
-                new{Nombre="Jorge", Dinero=900}
-            };
-
-            /*for (int i = 0; i <arrayEmpleados.Length; i++)
-            {
-                Console.WriteLine(arrayEmpleados[i].getInfo());
-            }*/
-
-            foreach (var variable in personas)
-            {
-                Console.WriteLine(variable);
+                Console.WriteLine(i);
 
             }
 
         }
+
+        static int[] LeerDatos()
+        {
+            Console.WriteLine("¿Cuántos elementos quieres que tenga el array?");
+
+            string respuesta = Console.ReadLine();
+
+            int numElementos = int.Parse(respuesta);
+
+            int[] datos = new int[numElementos];
+
+            for(int i=0; i<numElementos; i++)
+            {
+                Console.WriteLine($"Introduce el dato para la posición {i}");
+
+                respuesta = Console.ReadLine();
+
+                int datosElemento = int.Parse(respuesta);
+
+                datos[i] = datosElemento;
+            }
+
+            return datos;   
+        }
+
+       
 
 
     }
